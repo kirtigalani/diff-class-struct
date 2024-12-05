35074db33a17e4e8f08a9eddd7ce9d11b1eb7c53
kirtigalani <kittugalani@gmail.com>
2024-12-05 10:25:18 +0530

    Add project files.

---
+++ diff class struct/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace diff_class_struct
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Customer()
+        {
+
+        }
+    }
+    internal class Program : Customer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
